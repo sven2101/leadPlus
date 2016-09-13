f2caa1aec45e354eec33fba5ef7cd39ec84dfaf6
Andreas Foitzik <andreas.foitzik@live.com>
2016-09-13 10:13:42 +0200

    fixing notification service

--- frontend/src/components/Notification/controller/Notification.Service.ts
+++ frontend/src/components/Notification/controller/Notification.Service.ts
@@ -1,4 +1,5 @@
 /// <reference path="../../app/App.Resource.ts" />
+/// <reference path="../../Notification/model/Notification.Model.ts" />
 
 /*******************************************************************************
  * Copyright (c) 2016 Eviarc GmbH.
@@ -19,22 +20,24 @@ const NotificationServiceId: string = "NotificationService";
 
 class NotificationService {
 
-    private $inject = [NotificationResourceId, toasterId, $translateId];
+    private $inject = [NotificationResourceId, toasterId, $translateId, $rootScopeId];
 
     notificationResource;
     toaster;
     translate;
+    rootScope;
 
-    constructor(NotificationResource, toaster, translate) {
+    constructor(NotificationResource, toaster, translate, $rootScope) {
         this.notificationResource = NotificationResource.resource;
         this.toaster = toaster;
         this.translate = translate;
+        this.rootScope = $rootScope;
     }
 
     send(notification: Notification) {
         let self = this;
         console.log("Notification", notification);
-        this.notificationResource.send({id: }notification).$promise.then(function () {
+        this.notificationResource.send({id: this.rootScope}, notification).$promise.then(function () {
             self.toaster.pop("success", "", self.translate.instant("NOTIICATION_SEND"));
         }, function () {
             self.toaster.pop("error", "", self.translate.instant("NOTIICATION_SEND_ERROR"));
