13dede4db12e21250d011193240ea608acde6ba0
max-hermann <maxhermann1@yahoo.de>
2016-11-02 18:31:04 +0100

    NotificationService now throws Exception again

--- src/main/java/dash/notificationmanagement/business/NotificationService.java
+++ src/main/java/dash/notificationmanagement/business/NotificationService.java
@@ -110,8 +110,8 @@ public class NotificationService implements INotificationService {
 				throw new SMTPdoesntExistsException("No valid SMTP Data for this User");
 			}
 		} catch (Exception ex) {
-			// throw ex;
-			return;
+			throw ex;
+			// return;
 		}
 	}
 
