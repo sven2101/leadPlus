569e9aba30d4e222a79879517de6684fde57a7da
Sven Jaschkewitz <11869433+sven2101@users.noreply.github.com>
2016-11-16 21:43:33 +0100

    delete all client files

--- frontend/src/components/Common/model/Defer.Interface.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../../Common/model/Promise.Interface.ts" />
-interface IDefer<T> {
-    promise: IPromise<T>;
-    resolve: (T) => IPromise<T>;
-    reject: (T) => IPromise<T>;
-
-}
