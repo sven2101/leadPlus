e5212d00c630c17a277c00b58b0efc498d3c0e1b
Sven Jaschkewitz <11869433+sven2101@users.noreply.github.com>
2016-11-25 21:35:01 +0100

    delete all files

--- frontend/src/components/Common/model/Activity.Enum.ts
+++
@@ -1,7 +0,0 @@
-enum Activity {
-    OPEN = <any>"OPEN",
-    OFFER = <any>"OFFER",
-    SALE = <any>"SALE",
-    INCONTACT = <any>"INCONTACT",
-    DONE = <any>"DONE",
-}
--- frontend/src/components/Product/model/ProductState.ts
+++
@@ -1,7 +0,0 @@
-enum ProductState {
-    NEW = <any>"NEW",
-    USED_A = <any>"USED_A",
-    USED_B = <any>"USED_B",
-    USED_C = <any>"USED_C",
-    USED_D = <any>"USED_D"
-}
