be29de978a7742dfa3717ee70c030fcf8ec906ae
max-hermann <maxhermann1@yahoo.de>
2016-11-29 16:15:50 +0100

    EV-219 added gulp hint

--- frontend/gulp/gulp.html.js
+++ frontend/gulp/gulp.html.js
@@ -4,40 +4,70 @@
 var gulp = require('gulp'),
     minifyHTML = require('gulp-minify-html'),
     watch = require('gulp-watch'),
-    path = require('./gulp.path').path;
-  
+    path = require('./gulp.path').path,
+    htmlhint = require("gulp-htmlhint");
+
 
 
 var opts = {
     comments: false
 };
 
-gulp.task('html',['minify-index','minify-main','minify-logout','minify-components-view'],function () {
+gulp.task('html', ['minify-index', 'minify-main', 'minify-logout', 'minify-components-view'], function() {
     return null;
 });
 
-gulp.task('minify-index', function () {
+gulp.task('minify-index', ['validate-single-view'], function() {
     return gulp.src(path.index.src)
+
         .pipe(minifyHTML(opts))
+        .pipe(htmlhint.reporter())
         .pipe(gulp.dest(path.index.dst));
 });
 
-gulp.task('minify-main', function () {
+gulp.task('minify-main', ['validate-single-view'], function() {
     return gulp.src(path.main.src)
+
         .pipe(minifyHTML(opts))
+        .pipe(htmlhint.reporter())
         .pipe(gulp.dest(path.main.dst));
 });
 
-gulp.task('minify-logout', function () {
+gulp.task('minify-logout', ['validate-single-view'], function() {
     return gulp.src(path.logout.src)
+
         .pipe(minifyHTML(opts))
+        .pipe(htmlhint.reporter())
         .pipe(gulp.dest(path.logout.dst));
 });
 
-gulp.task('minify-components-view', function () {
+gulp.task('minify-components-view', ['validate-components-view'], function() {
     return gulp.src(path.componentViews.src)
+
         .pipe(minifyHTML(opts))
+        .pipe(htmlhint.reporter())
         .pipe(gulp.dest(path.componentViews.dst));
-       
-  });
+
+});
+
+gulp.task('validate-components-view', function() {
+    return gulp.src([
+        path.componentViews.src
+    ])
+        .pipe(htmlhint('.htmlhintrc'))
+        .pipe(htmlhint.reporter());
+})
+
+gulp.task('validate-single-view', function() {
+    return gulp.src([
+        path.index.src,
+        path.main.src,
+        path.logout.src,
+    ])
+        .pipe(htmlhint('.htmlhintrc'))
+        .pipe(htmlhint.reporter());
+})
+
+
+
 
