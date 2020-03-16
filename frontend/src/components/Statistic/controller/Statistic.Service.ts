0a007765c48a5879106496d94b6631d1b6b596c9
Max Hermann <maxhermann1@yahoo.de>
2020-03-16 13:50:45 +0100

    *removed remaining licence headers

--- frontend/src/components/Statistic/controller/Statistic.Service.ts
+++ frontend/src/components/Statistic/controller/Statistic.Service.ts
@@ -5,16 +5,6 @@
 /// <reference path="../../Statistic/model/AreaChart.Model.ts" />
 /// <reference path="../../Statistic/model/ColumnChart.Model.ts" />
 /// <reference path="../../Statistic/model/SplineChart.Model.ts" />
-/*******************************************************************************
- * Copyright (c) 2016 Eviarc GmbH. All rights reserved.
- * 
- * NOTICE: All information contained herein is, and remains the property of
- * Eviarc GmbH and its suppliers, if any. The intellectual and technical
- * concepts contained herein are proprietary to Eviarc GmbH, and are protected
- * by trade secret or copyright law. Dissemination of this information or
- * reproduction of this material is strictly forbidden unless prior written
- * permission is obtained from Eviarc GmbH.
- ******************************************************************************/
 "use strict";
 
 const StatisticServiceId: string = "StatisticService";
